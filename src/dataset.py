af5ff65c11a5e8069281b63932816d81c7dcc5c3
Philippa Richter <pips.richter@gmail.com>
2025-03-01 12:45:15 -0800

    memory weirdness

--- src/dataset.py
+++ src/dataset.py
@@ -12,14 +12,30 @@ DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 Datasets = namedtuple('Datasets', ['train', 'test'])
 
+# https://stackoverflow.com/questions/53097952/how-to-understand-numpy-strides-for-layman
+
+# TODO: Figure out why this error was happening.
+# Traceback (most recent call last):
+#   File "/central/groups/fischergroup/prichter/miniconda3/envs/tripy/bin/train", line 33, in <module>
+#     sys.exit(load_entry_point('tripy', 'console_scripts', 'train')())
+#              ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~^^
+#   File "/central/groups/fischergroup/prichter/tripy/src/cli.py", line 180, in train
+#     dataset = Dataset.from_hdf(args.input_path, feature_type=args.feature_type, load_seqs=True, load_labels=True)
+#   File "/central/groups/fischergroup/prichter/tripy/src/dataset.py", line 52, in from_hdf
+#     return cls(embeddings_df.values, labels=labels, seqs=seqs, index=embeddings_df.index.values, feature_type=feature_type, scaled=False)
+#   File "/central/groups/fischergroup/prichter/tripy/src/dataset.py", line 30, in __init__
+#     self.embeddings = torch.from_numpy(embeddings).to(DEVICE)
+#                       ~~~~~~~~~~~~~~~~^^^^^^^^^^^^
+# ValueError: given numpy array strides not a multiple of the element byte size. Copy the numpy array to reallocate the memory.
 
 class Dataset(torch.utils.data.Dataset):
 
-    def __init__(self, embeddings:np.ndarray, labels:np.ndarray=None, seqs:np.ndarray=None, index:np.ndarray=None, scaled:bool=False, feature_type:str=None):
+    def __init__(self, embeddings:np.ndarray, metadata:pd.DataFrame=None, labels:np.ndarray=None, seqs:np.ndarray=None, index:np.ndarray=None, scaled:bool=False, feature_type:str=None):
 
         self.labels = labels
         self.n_features = embeddings.shape[-1]
         self.feature_type = feature_type 
+        self.metadata = metadata 
 
         if (self.labels is not None):
             self.n_classes = len(np.unique(labels)) # Infer the number of classes based on the labels. 
@@ -39,17 +55,20 @@ class Dataset(torch.utils.data.Dataset):
 
 
     @classmethod
-    def from_hdf(cls, path:str, feature_type:str=None, load_seqs:bool=True, load_labels:bool=True):
+    def from_hdf(cls, path:str, feature_type:str=None, load_seqs:bool=True, load_labels:bool=True, load_metadata:bool=True):
         embeddings_df = pd.read_hdf(path, key=feature_type)
         metadata_df = pd.read_hdf(path, key='metadata')
         
         assert len(embeddings_df) == len(metadata_df), 'Dataset.from_hdf: The indices of the embeddings and the metadata do not match.'
         assert np.all(embeddings_df.index == metadata_df.index), 'Dataset.from_hdf: The indices of the embeddings and the metadata do not match.'
 
-        labels = metadata_df.label.values if load_labels else None
-        seqs = metadata_df.seq.values if load_seqs else None
+        kwargs = dict()
+        kwargs['labels'] = metadata_df.label.values if load_labels else None
+        kwargs['seqs'] = metadata_df.seq.values if load_seqs else None
+        kwargs['metadata'] = metadata_df if load_metadata else None
+        kwargs['index'] = metadata_df.index.values
 
-        return cls(embeddings_df.values, labels=labels, seqs=seqs, index=embeddings_df.index.values, feature_type=feature_type, scaled=False)
+        return cls(embeddings_df.values.copy(), feature_type=feature_type, scaled=False, **kwargs)
     
     def shape(self):
         return self.embeddings.shape
@@ -65,10 +84,11 @@ class Dataset(torch.utils.data.Dataset):
 
     def subset(self, idxs):
 
-        embeddings = self.embeddings.cpu().numpy()[idxs, :]
-        labels = self.labels.cpu().numpy()[idxs] if (self.labels is not None) else self.labels
-        seqs = self.seqs[idxs]
-        index = self.index[idxs]
+        embeddings = self.embeddings.cpu().numpy()[idxs, :].copy()  
+        labels = self.labels.cpu().numpy()[idxs] if (self.labels is not None) else None
+        metadata = self.metadata.iloc[idxs].copy() if (self.metadata is not None) else None
+        seqs = self.seqs[idxs].copy()
+        index = self.index[idxs].copy()
         scaled = self.scaled 
 
         return Dataset(embeddings, labels=labels, seqs=seqs, index=index, scaled=scaled, feature_type=self.feature_type)
