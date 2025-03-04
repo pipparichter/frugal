8a21331db599b53f3ff23e9e75c27428ec61c475
Philippa Richter <pips.richter@gmail.com>
2025-03-04 14:33:21 -0800

    fixing cli

--- src/cli.py
+++ src/cli.py
@@ -186,7 +186,7 @@ def train():
     loss_func_weights = [int(w) for w in args.loss_func_weights.split(',')] if (args.loss_func_weights is not None) else loss_func_weights
 
     dataset = Dataset.from_hdf(args.input_path, feature_type=args.feature_type, attrs=['seq', 'label', 'genome_id'])
-    model = Classifier(dims=(dataset.n_features, 512, dataset.n_classes), loss_func_weights=args.loss_func_weights)
+    model = Classifier(dims=(dataset.n_features, 512, dataset.n_classes), loss_func_weights=loss_func_weights)
     # I think I want to split along the genome IDs here as well, possibly even sample to make sure the class distribution is even.
     dataset_train, dataset_test = split(dataset, by='genome_id') 
     model.scale(dataset_train, fit=True)
