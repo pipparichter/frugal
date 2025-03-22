bfb5bc50b907753b32e220e145dd9178acb02db4
Philippa Richter <pips.richter@gmail.com>
2025-03-21 19:13:36 -0700

    weird bug

--- src/dataset.py
+++ src/dataset.py
@@ -113,8 +113,8 @@ class Dataset(torch.utils.data.Dataset):
 
     def subset(self, idxs):
         embedding = self.embedding.cpu().numpy()[idxs, :].copy()  
-        index = self.index.copy()[idxs]
-        kwargs = {attr:getattr(self, attr)[idxs] for attr in self.attrs}
+        index = self.index.copy()[idxs].copy()
+        kwargs = {attr:getattr(self, attr)[idxs].copy() for attr in self.attrs}
         return Dataset(embedding, index=index, scaled=self.scaled, feature_type=self.feature_type, **kwargs)
     
     def write(self, path:str):
