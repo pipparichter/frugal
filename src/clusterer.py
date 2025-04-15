dcc6a8b326649ba6989a46489f7b143fe78b35ea
Philippa Richter <pips.richter@gmail.com>
2025-04-15 11:24:16 -0700

    debug

--- src/clusterer.py
+++ src/clusterer.py
@@ -428,6 +428,7 @@ class Clusterer():
         for i in tqdm(range(self.n_clusters), desc='Clusterer.get_davies_bouldin_index'):
             sigma_i = np.repeat(sigma[i], k)
             sigma_j = sigma[nearest_cluster_ids[i]]
+            print(np.repeat(i, k), nearest_cluster_ids[i])
             distances = D._get_vectorized(np.repeat(i, k), nearest_cluster_ids[i])
             cluster_metadata_df['davies_bouldin_index'] = ((sigma_i + sigma_j) / distances).max(axis=None)
         cluster_metadata_df['intra_cluster_distance_center'] = sigma 
